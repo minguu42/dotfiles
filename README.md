025c28b5ac5ef455ab0b86ac9f698cfa74027553
Akira Furukawa <minguu42@gmail.com>
2024-11-18 21:08:03 +0900

    xcode-selectを対話的にインストールするように変更する

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 ## Installation
 
 ```bash
+xcode-select --install
+```
+
+```bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/minguu42/dotfiles/main/install.sh)"
 ```
--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if xcode-select -p >/dev/null 2>&1 ; then
-  echo "Command Line Tools for Xcode is already installed."
-else
-  echo "Installing Command Line Tools for Xcode..."
-  xcode-select --install
+if ! xcode-select -p >/dev/null 2>&1 ; then
+  echo "Error: xcode-select is not installed."
+  echo "Please run 'xcode-select --install' to install the command line tools for xcode."
+  exit 1
 fi
 
 INSTALL_DIR="${INSTALL_DIR:-$HOME/ghq/github.com/minguu42/dotfiles}"
@@ -17,6 +16,8 @@ else
   echo "Installing dotfiles..."
   git clone https://github.com/minguu42/dotfiles "$INSTALL_DIR"
 fi
+
+/bin/bash "$INSTALL_DIR/deploy.sh"
 
 if type brew >/dev/null ; then
   echo "Homebrew is already installed."
@@ -30,8 +31,6 @@ brew update
 
 echo "Installing dependencies from Homebrew and Homebrew Cask..."
 brew bundle install --file "$INSTALL_DIR/config/brew/.Brewfile" --no-lock
-
-/bin/bash "$INSTALL_DIR/deploy.sh"
 
 zsh_path="$(brew --prefix)/bin/zsh"
 if [[ -f "$zsh_path" && "$SHELL" != "$zsh_path" ]] ; then
