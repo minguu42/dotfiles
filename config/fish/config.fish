73cf8cbc07efc76fe836ffb42c369bd53862ebe9
Akira Furukawa <minguu42@gmail.com>
2025-03-21 14:28:19 +0900

    Gitのエイリアス設定をfishの略語に移す

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -23,6 +23,21 @@ abbr -ac docker m compose
 abbr -ac docker n network
 abbr -ac docker v volume
 abbr -a g git
+abbr -ac git ad add
+abbr -ac git br branch
+abbr -ac git cm commit
+abbr -ac git df diff
+abbr -ac git lg log -n 15
+abbr -ac git mg merge
+abbr -ac git ph push
+abbr -ac git pl pull --prune
+abbr -ac git rs reset
+abbr -ac git rt remote
+abbr -ac git rv revert
+abbr -ac git sm submodule
+abbr -ac git ss stash
+abbr -ac git st status
+abbr -ac git sw switch
 abbr -a m make
 
 ### キーバインド
