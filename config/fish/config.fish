f9aa6346bbfbb3ab01a0e98dae18d2dae27a8f9a
Akira Furukawa <minguu42@gmail.com>
2025-05-14 11:45:46 +0900

    Gitの略語の見直し

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -27,20 +27,22 @@ abbr -a d_m --regex m --command docker compose
 abbr -a d_n --regex n --command docker network
 abbr -a d_v --regex v --command docker volume
 abbr -a g git
-abbr -a g_ad --regex ad --command git add
-abbr -a g_br --regex br --command git branch
-abbr -a g_cm --regex cm --command git commit
-abbr -a g_df --regex df --command git diff
-abbr -a g_lg --regex lg --command git log -n 15
-abbr -a g_mg --regex mg --command git merge
-abbr -a g_ph --regex ph --command git push
+abbr -a g_a  --regex a  --command git add
+abbr -a g_b  --regex b  --command git branch
+abbr -a g_c  --regex c  --command git --set-cursor 'commit -m "%"'
+abbr -a g_d  --regex d  --command git diff
+abbr -a g_l  --regex l  --command git log -n 15
+abbr -a g_m  --regex m  --command git merge
+abbr -a g_p  --regex p  --command git push
 abbr -a g_pl --regex pl --command git pull --prune
-abbr -a g_rs --regex rs --command git reset
+abbr -a g_rb --regex rb --command git rebase
 abbr -a g_rt --regex rt --command git remote
+abbr -a g_rs --regex rs --command git reset
+abbr -a g_r  --regex r  --command git restore
 abbr -a g_rv --regex rv --command git revert
 abbr -a g_sm --regex sm --command git submodule
 abbr -a g_ss --regex ss --command git stash
-abbr -a g_st --regex st --command git status
+abbr -a g_s  --regex s  --command git status
 abbr -a g_sw --regex sw --command git switch
 abbr -a gh_c --regex c --command gh create
 abbr -a gh_m --regex m --command gh merge
